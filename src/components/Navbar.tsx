7ed1ce36bd02b78b780b34343939c0b1e24a11f8
Anh Le <anh4dle@gmail.com>
2023-10-26 17:28:14 -0400

    vibedesk logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { AiFillHome, AiOutlineFolderView } from "react-icons/ai";
 import { BsFillPeopleFill } from "react-icons/bs";
+import { TbCircleLetterV } from "react-icons/tb";
 
 function Navbar() {
   return (
-    <div className="flex flex-col justify-between h-screen text-3xl bg-teal-900 w-16">
-      <div className="[&>div]:text-gray-300 [&>div]:hover:cursor-pointer  w-full [&>div]:w-full [&>div]:mb-4">
-        <div className="hover:text-gray-50 flex flex-col items-center">
+    <div className="flex flex-col justify-between h-screen text-3xl bg-cyan-800	w-16 [&>div]:text-gray-300 [&>div]:hover:cursor-pointer ">
+      <div className=" w-full [&>div]:w-full [&>div]:mt-4">
+        <div className="hover:text-gray-50 flex justify-center">
           <AiFillHome />
         </div>
-        <div className="hover:text-gray-50 flex flex-col items-center">
+        <div className="hover:text-gray-50 flex justify-center">
           <AiOutlineFolderView />
         </div>
-        <div className="hover:text-gray-50 flex flex-col items-center">
+        <div className="hover:text-gray-50 flex justify-center">
           <BsFillPeopleFill />
         </div>
       </div>
-      <div>Zen</div>
+      <div className="mb-4 flex justify-center text-white text-4xl">
+        <TbCircleLetterV />
+      </div>
     </div>
   );
 }
