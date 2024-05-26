3408ebed7cd4c6e1ec874307454cc55832e47475
Anh Le <anh4dle@gmail.com>
2024-05-26 19:43:13 +0700

    working on adding ticket

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,19 @@
 import { useContext, useState } from "react";
 import Button from "./Button";
 import { AiOutlineCloseCircle } from "react-icons/ai";
-import { TicketDataContext, Ticket } from "../contexts/TicketDataContext";
+import { TicketDataContext } from "../contexts/TicketDataContext";
 
 function Form() {
-  const { ticketsState, page } = useContext(TicketDataContext);
+  const { ticketsState } = useContext(TicketDataContext);
   const [ticket, setTicketVal] = useState({
+    id: 24,
     name: "",
     subject: "",
+    created: "",
     status: "",
     priority: "",
     description: "",
+    agent: "Anh",
     messages: [
       {
         message: "Lorem ipsum dolor sit",
@@ -21,15 +24,26 @@ function Form() {
     ],
     checked: false,
   });
-  const handleChange = (e) => {
+
+  const handleChange = (e: any) => {
     setTicketVal({
-      ...ticketVal,
+      ...ticket,
       [e.target.name]: e.target.value,
     });
   };
   const createTicket = () => {
+    const newDate = new Date();
+    const date = newDate.getDate();
+    const month = newDate.getMonth();
+    const year = newDate.getFullYear();
+
+    const timestampStr = `${month}/${date}/${year}`;
+    console.log(timestampStr);
+
+    ticket.created = timestampStr;
     ticketsState.push(ticket);
   };
+  console.log(ticket);
   return (
     <div className="text-sm flex flex-col p-4 gap-4 [&>input]:border [&>input]:p-2 [&>select]:border [&>select]:p-2">
       <div className="flex justify-between items-center text-xl">
@@ -41,31 +55,35 @@ function Form() {
       <input
         type="text"
         id="name"
+        name="name"
         value={ticket.name}
         onChange={handleChange}
       />
-
-      <label htmlFor="email">Email</label>
-      <input type="text" id="email" />
 
       <label htmlFor="subject">Subject</label>
       <input
         type="text "
         id="subject"
+        name="subject"
         value={ticket.subject}
         onChange={handleChange}
       />
 
       <label htmlFor="status">Status:</label>
 
-      <select name="status" id="status">
+      <select name="status" id="status" value={ticket.status}>
         <option value="open">Open</option>
         <option value="pending">Pending</option>
         <option value="resolved">Resolved</option>
       </select>
       <label htmlFor="priority">Priority:</label>
 
-      <select name="priority" id="priority">
+      <select
+        name="priority"
+        id="priority"
+        value={ticket.priority}
+        onChange={handleChange}
+      >
         <option value="low">Low</option>
         <option value="medium">Medium</option>
         <option value="high">High</option>
@@ -75,6 +93,7 @@ function Form() {
       <textarea
         className="border p-2 grow h-26 "
         id="description"
+        name="description"
         value={ticket.description}
         onChange={handleChange}
       />
